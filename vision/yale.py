e92033541027a659d4e300737dc8990fbea83dc9
kingofspace0wzz <zizhuang_wang@umail.ucsb.edu>
2019-05-22 14:35:27 -0700

    u

--- vision/yale.py
+++ vision/yale.py
@@ -76,10 +76,11 @@ def run(args, data_iter, model, clf, optimizers, epoch, train=True, pretrain=Fal
 
         if train:
             optimizer_phi.zero_grad()
-            # phi = model.phi(z.detach())
-            # neg_h = -HSIC(phi, light)
-            neg_h = -total_loss
-            neg_h.backward(retain_graph=True)
+            phi = model.phi(z.detach())
+            neg_h = -HSIC(phi, light)
+            # neg_h = -total_loss
+            # neg_h.backward(retain_graph=True)
+            neg_h.backward()
             optimizer_phi.step()
             optimizer.zero_grad()
             total_loss.backward()
